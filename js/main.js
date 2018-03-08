5af549eee89e11cc84e4d1cc2385a598cbed7b29
Łukasz Wac <wac.lukasz@gmail.com>
2018-03-08 01:31:11 +0100

    loader-animiation-added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+loader();
+//headerAnimation();
+
 // Expand the main navigation menu
 $('.nav-toggler').on('click', expandMenu);
 
@@ -10,9 +13,11 @@ $('.nav-item > a').on('click', function (e) {
     $('html, body').delay(300).animate({
         scrollTop: $target.offset().top
     }, 500);
-    
+
     expandMenu();
 });
+
+$(document).on('scroll', headerAnimation);
 
 function expandMenu() {
     let $menu = $('.menu-container');
@@ -52,3 +57,75 @@ function expandMenu() {
         }
     }
 };
+
+function loader() {
+    let $loaderBox = $('.loader');
+    let $loaderStart = $('.loader-start');
+    let $loaderCover = $('.loader-cover');
+
+    $('html, body').scrollTop(0);
+
+    $('html, body').css({
+        overflow: "hidden"
+    });
+
+    $loaderStart.delay(600).animate({
+        marginTop: "0",
+        opacity: "1"
+    }, 800, "swing", function () {
+        $loaderCover.delay(400).animate({
+            height: "0vh"
+        }, 600, "swing");
+        $loaderStart.delay(400).animate({
+            marginTop: "10",
+            opacity: "0"
+        }, 400, "swing", function () {
+            $loaderBox.delay(200).animate({
+                height: "0vh"
+            }, 300, function () {
+                $loaderBox.css({
+                    display: "none"
+                });
+            });
+            headerLoader();
+            smallHeaderLoader();
+        });
+    });
+};
+
+function headerLoader() {
+    let $headerItems = $('.head > span');
+
+    $headerItems.delay(200).animate({
+        opacity: "1",
+        margin: "0"
+    }, 900, "swing", function () {
+        $('html, body').css({
+            overflow: "auto"
+        });
+    });
+};
+
+function smallHeaderLoader() {
+    let $border = $('.border');
+    let $smallHeader = $('.small-head > h2');
+
+    $border.delay(400).animate({
+        height: "25px",
+        opacity: "1"
+    }, 800, "swing");
+    $smallHeader.delay(600).animate({
+        opacity: "1"
+    }, 800);
+}
+
+function headerAnimation() {
+    let $headerItems = $('.head > span');
+    let scrollTop = $('html, body').scrollTop();
+
+    if (scrollTop < $('#main-header').height()) {
+        $headerItems.css({
+            margin: scrollTop / 25 // to make marginTop value smaller
+        });
+    };
+}
